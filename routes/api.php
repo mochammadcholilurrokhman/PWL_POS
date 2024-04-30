a57de593d97e7affc76632fd92decac2d5e43c34
mochammadcholilurrokhman <ilulsrut69@gmail.com>
2024-04-30 22:11:11 +0700

    Jobsheet 10

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,8 @@ use App\Http\Controllers\Api\LoginController;
 */
 
 Route::post('/register', RegisterController::class);
-Route::post('/login', LoginController::class);
+Route::post('/login', LoginController::class); 
+Route::post('/logout', LogoutController::class); 
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
